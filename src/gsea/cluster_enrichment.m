c708ac55a756b27186eb0e9f92da786792ca9dfd
Benj Shapiro <benj.shapiro@gmail.com>
2016-05-16 20:51:54 -0400

    added cluster_enrichment matlab script

---
+++ src/gsea/cluster_enrichment.m
@@ -0,0 +1,14 @@
+genes = load('../data/myeloma_genes.mat');
+clusters = load('../data/myeloma_pca_clusters.mat');
+cluster_names = fieldnames(clusters);
+for i=1:numel(cluster_names)
+    cluster = clusters.(cluster_names{i});
+    [enrich_data, enrich_genes] = geneset_enrichment(genes.genes, cluster, 'GO-BP', strcat('../data/gsea_output/svd_', char(cluster_names{i})), 20);
+end
+
+clusters = load('../data/myeloma_cca_clusters.mat');
+cluster_names = fieldnames(clusters);
+for i=1:numel(cluster_names)
+    cluster = clusters.(cluster_names{i});
+    [enrich_data, enrich_genes] = geneset_enrichment(genes.genes, cluster, 'GO-BP', strcat('../data/gsea_output/cca_', char(cluster_names{i})), 20);
+end
