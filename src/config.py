5dc48a156662da5365804c9ed9883653e8eac2a0
Benj Shapiro <benj.shapiro@gmail.com>
2016-05-23 01:13:41 -0400

    cleaned up config

--- src/config.py
+++ src/config.py
@@ -3,13 +3,10 @@ config = dict(
     cca_components=3,
     kmeans_clusters=71,
     scale_data=1,
-    preprocessing='raw',
-    representation='pca',
     time_transform=False,
     gp_type='rbf',
     gp_lengthscale=5,
     gp_variance=4,
-    sparse_pca_alpha=1,
-    clean_components=4,
+    clean_components=3,
     clean_data=True
 )
