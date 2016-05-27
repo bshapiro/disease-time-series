8b68cac59ea04e3d011fe705c668862f793cf216
Benj Shapiro <benj.shapiro@gmail.com>
2016-05-27 01:49:01 -0400

    updated cluster enrichment to handle multiple clusterings at a time

--- src/gsea/cluster_enrichment.m
+++ src/gsea/cluster_enrichment.m
@@ -1,14 +1,26 @@
-genes = load('../../data/myeloma_genes.mat');
-clusters = load('../../data/myeloma_pca_clusters.mat');
-cluster_names = fieldnames(clusters);
-for i=1:numel(cluster_names)
-    cluster = clusters.(cluster_names{i});
-    [enrich_data, enrich_genes] = geneset_enrichment(genes.genes, cluster, 'GO-BP', strcat('../../data/gsea_output/svd_', char(cluster_names{i})), 20);
-end
+folder_dir = '../../data/gsea_output/';
+folders = dir(strcat(folder_dir, 'cleaned*'));
+isub = [folders(:).isdir]; %# returns logical vector
+nameFolds = {folders(isub).name}';
+genes = load(strcat(folder_dir, 'myeloma_genes.mat'));
+for i=1:numel(nameFolds)
+    pca_file = dir(strcat(folder_dir, nameFolds{i}, '/myeloma_pca*.mat'));
+    pca_filename = pca_file.name;
+    clusters = load(strcat(folder_dir, nameFolds{i}, '/', pca_filename));
 
-clusters = load('../../data/myeloma_cca_clusters.mat');
-cluster_names = fieldnames(clusters);
-for i=1:numel(cluster_names)
-    cluster = clusters.(cluster_names{i});
-    [enrich_data, enrich_genes] = geneset_enrichment(genes.genes, cluster, 'GO-BP', strcat('../../data/gsea_output/cca_', char(cluster_names{i})), 20);
+    cluster_names = fieldnames(clusters);
+    for j=1:numel(cluster_names)
+        cluster = clusters.(cluster_names{j});
+        [enrich_data, enrich_genes] = geneset_enrichment(genes.genes, cluster, 'GO-BP', strcat(folder_dir, nameFolds{i}, '/svd_', char(cluster_names{j})), 20);
+    end
+    
+    cca_file = dir(strcat(folder_dir, nameFolds{i}, '/myeloma_cca*.mat'));
+    cca_filename = cca_file.name;
+    clusters = load(strcat(folder_dir, nameFolds{i}, '/', cca_filename));
+
+    cluster_names = fieldnames(clusters);
+    for j=1:numel(cluster_names)
+        cluster = clusters.(cluster_names{j});
+        [enrich_data, enrich_genes] = geneset_enrichment(genes.genes, cluster, 'GO-BP', strcat(folder_dir, nameFolds{i}, '/cca_', char(cluster_names{j})), 20);
+    end
 end
