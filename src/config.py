65e6e88b6bd0b3fb52b3e240df42c0764211fc67
Benj Shapiro <benj.shapiro@gmail.com>
2016-06-16 16:22:36 -0400

    added several config options

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 config = dict(
+    project_root='/Users/benj/Documents/Research/Projects/disease-time-series/',
+    representation='pca',
     pca_components=3,
     cca_components=3,
     kmeans_clusters=71,
@@ -8,5 +10,6 @@ config = dict(
     gp_variance=4,
     clean_components=3,
     clean_data=True,
-    cca_reg=0.00001
+    cca_reg=0.0001,
+    view_pca_plots=False
 )
