80c76be226ce6c213f1478399a49e9ec7486c0ba
karltayeb <karl.tayeb@gmail.com>
2016-06-17 14:34:58 -0400

    brought in changes from other branch

--- src/tools/helpers.py
+++ src/tools/helpers.py
@@ -25,6 +25,29 @@ def plot_2D(data, color_labels=None, x=0, y=1, xlabel='X', ylabel='Y', title='2-
     plt.xlabel(xlabel, figure=fig)
     plt.ylabel(ylabel, figure=fig)
 
+def QQPlot(observed_p, savename):
+    # make the QQ plot, borrowed from Princy's code demonstrating QQ plots
+    n = observed_p.size
+    obs_p = -np.log10(np.sort(observed_p))
+    th_p = np.arange(1/float(n),1+(1/float(n)),1/float(n))
+    th_p = -np.log10(th_p)
+    fig, ax = plt.subplots(ncols=1)
+    ax.scatter(th_p,obs_p)
+    x = np.linspace(*ax.get_xlim())
+    ax.plot(x, x)
+    ax.set_xlabel('Theoretical')
+    ax.set_ylabel('Observed')
+    ax.set_title(savename)
+    plt.savefig(savename)
+
+def benjamini(x, p):
+    x = np.sort(x.flatten())
+    p = p / x.size
+    significant = np.empty(x.size, dtype=bool)
+    for i in range(x.size):
+        significant[i] = x[i] < (i+1) * p
+return significant
+
 def label_coloring(labels):
     """
     returns a coloring where each unique label gets its own color
@@ -57,33 +80,76 @@ def get_lsv(X, pca):
     return U, SV, W
 
 
-    def regress_out(self,ind, X=None, axis=None):
-        if X is None:
-            X = self.X
-        if axis is None:
-            # try and figure out what axis based on the dimensions of independent variable
-            n = ind.shape[0]
-            if X.shape[0] == X.shape[1]:
-                # this is an ambigous case
-                print >> sys.stderr, 'Ambiguous dimensions, please specify axis'
-                return
-            if X.shape[0] == n:
-                axis = 0
-            elif X.shape[1] == n:
-                axis = 1
-            else:
-                print >> sys.stderr, 'Mismatched dimensions'
-                
-        print 'Axis: ', axis
-        if axis == 1:
-            X = np.transpose(X)
-        linreg = LinearRegression()
-        linreg.fit(ind, X)
-        new_data = X - linreg.predict(ind)
-        # add back the intercept
-        new_data = np.apply_along_axis(np.add, 1, new_data, linreg.intercept_)
-        if axis == 1:
-              X = np.transpose(X)
-              new_data = np.transpose(new_data)
-        return new_data, linreg
+def regress_out(self,ind, X=None, axis=None):
+    if X is None:
+        X = self.X
+    if axis is None:
+        # try and figure out what axis based on the dimensions of independent variable
+        n = ind.shape[0]
+        if X.shape[0] == X.shape[1]:
+            # this is an ambigous case
+            print >> sys.stderr, 'Ambiguous dimensions, please specify axis'
+            return
+        if X.shape[0] == n:
+            axis = 0
+        elif X.shape[1] == n:
+            axis = 1
+        else:
+            print >> sys.stderr, 'Mismatched dimensions'
+            
+    print 'Axis: ', axis
+    if axis == 1:
+        X = np.transpose(X)
+    linreg = LinearRegression()
+    linreg.fit(ind, X)
+    new_data = X - linreg.predict(ind)
+    # add back the intercept
+    new_data = np.apply_along_axis(np.add, 1, new_data, linreg.intercept_)
+    if axis == 1:
+          X = np.transpose(X)
+          new_data = np.transpose(new_data)
+    return new_data, linreg
+
+def associate(data1, data2, targets1=None, targets2=None, outpath=''):
+    """
+    data1 (ixj)and data2(nxm) are DataFrames
+    targets are the columns of data 2 that we want to check an
+    associationg for with the rows of data1
+    returns an ixm dataframe with benjamini hochberg corrected p values of spearman correlations
+    """
+    print 'Finding Associations...'
+    if targets1 is None:
+        targets1 = data1.axes[0] 
+    if targets2 is None:
+        targets2 = data2.axes[1]
+
+    output = pd.DataFrame(data=np.empty((targets1.size, targets2.size)), index=targets1, columns=targets2)
+    
+    for j in targets2:
+        for i in targets1:
+            notnan = data2[(~np.isnan(data2[j].astype(float)))].index
+            r, p = sp.stats.spearmanr(data1.loc[i, notnan].as_matrix(), data2.loc[notnan,j].as_matrix())
+            output.loc[i,j] = p
+        savepath = outpath+j
+        QQPlot(output[j].as_matrix(), savename=savepath)
+        
+    return output
+
+def check_k_range(data, cluster_sizes, savename):
+    for k in cluster_sizes:
+        print 'k= :', k
+        index_pairs = defaultdict(int)
+        for repeat in range(2):
+        km = KMeans(n_clusters=k)
+        km.fit(data)
+        l = km.labels_
+        for i in range(k):
+            pairs = set(itertools.combinations(np.where(l==i)[0].tolist(), 2))
+            for pair in pairs:
+                index_pairs.__getitem__(pair)
+                index_pairs[pair] += 1
+                conservation.append(sum(index_pairs.values()) / float((len(index_pairs.keys()) * 2)))
+
+    dump(open(savename, 'wb'))
+    print conservation
 
