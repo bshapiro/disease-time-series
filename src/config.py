d1d18498438273b4b7cc1b2deb25c7b73535fd5a
karltayeb <karl.tayeb@gmail.com>
2016-06-20 16:37:41 -0400

    changed parameter values

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ config = dict(
     representation='pca',
     pca_components=3,
     cca_components=3,
-    kmeans_clusters=71,
+    kmeans_clusters=115,
     time_transform=False,
     gp_type='rbf',
     gp_lengthscale=5,
@@ -12,5 +12,6 @@ config = dict(
     clean_data=True,
     cca_reg=0.0001,
     view_pca_plots=False,
-    transpose_data=False
+    transpose_data=False,
+    filter_include_nan=True,
 )
