e0d737d8a011c849afa496ad19576ae3f03d3fa0
karltayeb <karl.tayeb@gmail.com>
2016-07-19 11:52:12 -0400

    script to train with viterbi algorithm

---
+++ hmm/train_vit.sh
@@ -0,0 +1,6 @@
+python random_init.py viterbi
+python pathway_init.py viterbi not-fixed
+python pathway_init.py viterbi fixed
+python random_init_invert.py viterbi
+python pathway_init_invert.py viterbi not-fixed
+python pathway_init_invert.py viterbi fixed 
