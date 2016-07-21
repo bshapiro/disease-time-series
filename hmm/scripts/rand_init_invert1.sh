ff862f48db421dc5c4621695eee1d44f23a5cbfb
karltayeb <karl.tayeb@gmail.com>
2016-07-20 23:18:23 -0400

    hhpc run scripts

---
+++ hmm/scripts/rand_init_invert1.sh
@@ -0,0 +1,10 @@
+# alg = sys.argv[1]
+# m = int(sys.argv[2])
+# k = int(sys.argv[3])
+# state_range = [int(i) for i in sys.argv[4:]]
+# k_range = [25, 50, 75, 100, 125]
+# state_range = [10, 20, 40, 60]
+
+python random_init_invert.py viterbi 6000 25 10 20 40 60
+python random_init_invert.py viterbi 6000 75 10 20 40 60
+python random_init_invert.py viterbi 6000 125 10 20 40 60
---
+++ hmm/scripts/rand_init_invert2.sh
@@ -0,0 +1,9 @@
+# alg = sys.argv[1]
+# m = int(sys.argv[2])
+# k = int(sys.argv[3])
+# state_range = [int(i) for i in sys.argv[4:]]
+# k_range = [25, 50, 75, 100, 125]
+# state_range = [10, 20, 40, 60]
+
+python random_init_invert.py viterbi 6000 50 10 20 40 60
+python random_init_invert.py viterbi 6000 100 10 20 40 60
