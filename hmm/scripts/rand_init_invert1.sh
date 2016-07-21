dbd1e922939a534c3851c8c902414b2a5d05bd3a
karltayeb <karl.tayeb@gmail.com>
2016-07-20 23:19:21 -0400

    added cd line

--- hmm/scripts/rand_init_invert1.sh
+++ hmm/scripts/rand_init_invert1.sh
@@ -4,7 +4,7 @@
 # state_range = [int(i) for i in sys.argv[4:]]
 # k_range = [25, 50, 75, 100, 125]
 # state_range = [10, 20, 40, 60]
-
+cd /home/ktayeb1/disease-time-series/hmm
 python random_init_invert.py viterbi 6000 25 10 20 40 60
 python random_init_invert.py viterbi 6000 75 10 20 40 60
 python random_init_invert.py viterbi 6000 125 10 20 40 60
--- hmm/scripts/rand_init_invert2.sh
+++ hmm/scripts/rand_init_invert2.sh
@@ -4,6 +4,6 @@
 # state_range = [int(i) for i in sys.argv[4:]]
 # k_range = [25, 50, 75, 100, 125]
 # state_range = [10, 20, 40, 60]
-
+cd /home/ktayeb1/disease-time-series/hmm
 python random_init_invert.py viterbi 6000 50 10 20 40 60
 python random_init_invert.py viterbi 6000 100 10 20 40 60
