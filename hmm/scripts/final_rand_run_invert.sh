e4541b06ff04768e99313e0e6f4357391ec848f7
karltayeb <karl.tayeb@gmail.com>
2016-07-23 13:51:43 -0400

    run with invert

---
+++ hmm/scripts/final_rand_run_invert.sh
@@ -0,0 +1,5 @@
+qsub -l nodes=1:ppn=2,mem=8gb,walltime=36:00:00 frri1.sh
+qsub -l nodes=1:ppn=2,mem=8gb,walltime=36:00:00 frri2.sh
+qsub -l nodes=1:ppn=2,mem=8gb,walltime=36:00:00 frri3.sh
+qsub -l nodes=1:ppn=2,mem=8gb,walltime=36:00:00 frri4.sh
+qsub -l nodes=1:ppn=2,mem=8gb,walltime=36:00:00 frri5.sh
