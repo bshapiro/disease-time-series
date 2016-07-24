e0a8b1e2666db1562502c653ab975f1878bad4fc
karltayeb <karl.tayeb@gmail.com>
2016-07-24 12:14:05 -0400

    evaluation scripts

---
+++ hmm/scripts/run_null_cluster_eval.sh
@@ -0,0 +1,14 @@
+python eval/stability_eval.py ../results/final/final_rand/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand/ 1 null row
+
+python eval/stability_eval.py ../results/final/final_rand_invert/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand_invert/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand_invert/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand_invert/ 1 null row
+python eval/stability_eval.py ../results/final/final_rand_invert/ 1 null row
+
+python eval/stability_eval.py ../results/final/timevariant/ 1 null row
+
---
+++ hmm/scripts/run_true_cluster_eval.sh
@@ -0,0 +1,13 @@
+python eval/run_cluster_eval.py ../results/final/final_rand/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand/ 1 true row
+
+python eval/run_cluster_eval.py ../results/final/final_rand_invert/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand_invert/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand_invert/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand_invert/ 1 true row
+python eval/run_cluster_eval.py ../results/final/final_rand_invert/ 1 true row
+
+python eval/run_cluster_eval.py ../results/final/timevariant/ 1 true row
