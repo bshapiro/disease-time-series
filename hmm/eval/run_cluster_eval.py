9b32f95ec9bbeaa7e8031f96fd1ccbe96507499b
karltayeb <karl.tayeb@gmail.com>
2016-07-26 13:48:14 -0400

    updates

--- hmm/eval/run_cluster_eval.py
+++ hmm/eval/run_cluster_eval.py
@@ -7,24 +7,31 @@ import hmm.khmm as khmm
 from hmm.load_data import load_data
 import glob
 import sys
-from pickle import dump
+import time
+from pickle import dump, load
 
 
-def prep(cluster_directory_root, depth, plottype='row'):
+def prep(cluster_directory_root, depth, genefile):
 
     # load data
     gc, mt, track = load_data(None, 0)
+    genes = load(open(genefile, 'r'))
+    gc.data = gc.data.loc[genes, :]
+
     data = pd.concat([gc.data, mt.data])
 
     labels = data.index.values
+    original_labels = labels
     pos_labels = labels + '+'
     neg_labels = labels + '-'
     pos_data = pd.DataFrame(data=data.as_matrix(), index=pos_labels,
                             columns=data.columns.values)
-    neg_data = pd.DataFrame(data=data.as_matrix(), index=neg_labels,
+    neg_data = pd.DataFrame(data=(data.as_matrix() * -1), index=neg_labels,
                             columns=data.columns.values)
 
     data = pd.concat([data, pos_data, neg_data])
+
+    print data.index.values
 
     generic_dir = cluster_directory_root.split('/') + (['*'] * depth)
     generic_dir = ('/').join(generic_dir)
@@ -44,6 +51,8 @@ def prep(cluster_directory_root, depth, plottype='row'):
             while l < len(lines):
                 cluster_name = lines[l]
                 cluster_members = lines[l + 1].split('\t')
+                if cluster_members == ['']:
+                    cluster_members = []
                 clusters[cluster_name] = cluster_members
                 l += 4
 
@@ -64,22 +73,21 @@ def prep(cluster_directory_root, depth, plottype='row'):
         except:
             pass
 
+    """
     background = set()
     for clustering in clusterings.itervalues():
         for cid, members in clustering.iteritems():
             background.update(set(members))
+    """
 
-    background = list(background)
+    background = list(original_labels)
     # data = data.loc[background, :]
 
     # generate ranomd clusterings of the same size k as our models
     random_clusterings = {}
-
+    np.random.seed(int(time.time()))
     for clustering_id, clustering in clusterings.iteritems():
         source = np.array(background)
-        random_assignments = np.random.choice(len(clustering), source.size)
-        random_assignments = np.random.choice(len(clustering), source.size)
-        random_assignments = np.random.choice(len(clustering), source.size)
         random_assignments = np.random.choice(len(clustering), source.size)
         random_clusters = {}
         for i, cluster_id in enumerate(clustering.iterkeys()):
@@ -87,17 +95,45 @@ def prep(cluster_directory_root, depth, plottype='row'):
                 source[np.where(random_assignments == i)[0]].tolist()
         random_clusterings[clustering_id] = random_clusters
 
-    return clusterings, random_clusterings, clusterings_models, data
+    # generate random signed clustering
+    random_signed_clusterings = {}
+    pn = np.array(['+', '-'])
+    for clustering_id, clustering in clusterings.iteritems():
+        source = np.array(background)
+        random_assignments = np.random.choice(len(clustering), source.size)
+        random_clusters = {}
+        for i, cluster_id in enumerate(clustering.iterkeys()):
+            members = source[np.where(random_assignments == i)[0]].tolist()
+            signed_members = []
+            for member in members:
+                sign = np.random.choice(pn, 1)[0]
+                signed_members.append(member + sign)
+
+            random_clusters[cluster_id] = signed_members
+        random_signed_clusterings[clustering_id] = random_clusters
+
+    return clusterings, random_clusterings, random_signed_clusterings,\
+        clusterings_models, data, original_labels
 
 
-def dunn_davies_plots(cluster_directory_root, clusterings, clusterings_models,
-                      data, prefix, plottype):
+def evaluations(cluster_directory_root, clusterings, clusterings_models,
+                      data, prefix, original_labels):
         # run dunn and davies_bouldin for clusterings and random permutations
-        logprob = report_total_logprob(clusterings, clusterings_models,
-                                         data)
+        logprob = report_avg_logprob(clusterings, clusterings_models,
+                                     data, original_labels)
         savename = cluster_directory_root + prefix + '_log_probs'
         dump(logprob, open(savename, 'w'))
 
+        # run dunn and davies_bouldin for clusterings and random permutations
+        intradists, avgintradists = report_intradist(clusterings, clusterings_models,
+                                     data, original_labels)
+        savename = cluster_directory_root + prefix + '_intradists'
+        dump(intradists, open(savename, 'w'))
+        savename = cluster_directory_root + prefix + '_avgintradists'
+        dump(avgintradists, open(savename, 'w'))
+
+
+        """
         dunn = report_dunn(clusterings, clusterings_models, data)
         savename = cluster_directory_root + prefix + '_dunn_index'
         dump(dunn, open(savename, 'w'))
@@ -107,7 +143,7 @@ def dunn_davies_plots(cluster_directory_root, clusterings, clusterings_models,
         dump(davies, open(savename, 'w'))
 
         if plottype == 'none':
-            pass
+            pass"""
 
         """
         elif plottype == 'kn_grid':
@@ -260,7 +296,7 @@ def report_davies_bouldin(clusterings, clusterings_models, data):
     return davies_bouldin
 
 
-def report_total_logprob(clusterings, clusterings_models, data):
+def report_avg_logprob(clusterings, clusterings_models, data, original_labels):
     """
     Liklihood of the entire model collection normalized by the number
     of succesfull calculations. In case of random assignment sometimes
@@ -277,6 +313,7 @@ def report_total_logprob(clusterings, clusterings_models, data):
             if model_id == 'noise' and noise_model is None:
                 noise_model = models[model_id]
             model = models[model_id]
+            print model.summarize(data.loc[members, :].as_matrix())
             for member in members:
                 try:
                     lp = model.log_probability(data.loc[member, :].as_matrix())
@@ -288,28 +325,66 @@ def report_total_logprob(clusterings, clusterings_models, data):
 
         print missed, ' missed out of: ', (missed + count)
         try:
-            alp = total
+            alp = total / count
         except:
             alp = 0
+
+        print clustering_id, total
+        print alp
         logprobs[clustering_id] = alp
 
-    logprobs['NOISE'] = noise_model.summarize(data.as_matrix())
+    logprobs['NOISE'] = noise_model.summarize(data.loc[original_labels, :].as_matrix()) / (missed + count)
 
     return logprobs
+
+def report_intradist(clusterings, clusterings_models, data, original_labels):
+    intradists = {}
+    avgintradists = {}
+    for clustering_id, clustering in clusterings.iteritems():
+        total = 0
+        count = 0
+        dists = {}
+        models = clusterings_models[clustering_id]
+        intra_func = khmm.intra_distance
+        intra_args = {'distance_func': khmm.viterbi_distance,
+                      'models': models, 'data': data, 'stat': 'mean'}
+        for model_id, members in clustering.iteritems():
+            intra_dist = intra_func(model_id, members, **intra_args)
+            dists[model_id] = intra_dist
+            if intra_dist is not None:
+                total += intra_dist
+                count += 1
+        if count > 0:
+            aid = total / count
+        else:
+            aid = -1e1000
+        avgintradists[clustering_id] = aid
+        intradists[clustering_id] = dists
+
+    return intradists, avgintradists
 
 if __name__ == "__main__":
     directory = sys.argv[1]
     depth = int(sys.argv[2])
     cluster_version = sys.argv[3]
-    plottype = sys.argv[4]
+    genefile = sys.argv[4]
     print 'genes'
     print directory
-    clusterings, random_clusterings, clusterings_models, data = \
-        prep(directory, depth, plottype)
-    if cluster_version == 'null':
+    clusterings, random_clusterings, random_signed, clusterings_models, data,\
+        original_labels = prep(directory, depth, genefile)
+    if cluster_version == 'rand':
+        print 'Calculating clutser metrics on RANDOM Clusterings'
         c = random_clusterings
         prefix = 'RANDOM'
-    else:
+
+    if cluster_version == 'signednull':
+        print 'Calculating clutser metrics on RANDOM Clusterings'
+        c = random_signed
+        print c
+        prefix = 'RANDOM'
+
+    if cluster_version == 'true':
+        print 'Calculating clutser metrics on TRUE Clusterings'
         c = clusterings
         prefix = 'TRUE'
-    dunn_davies_plots(directory, c, clusterings_models, data, prefix, plottype)
+    evaluations(directory, c, clusterings_models, data, prefix, original_labels)
