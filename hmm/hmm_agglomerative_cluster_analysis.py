bbd52599c08214562f3bb33ffd351130786ec56d
karltayeb <karl.tayeb@gmail.com>
2016-12-12 23:23:26 -0500

    saves pickle

--- hmm/hmm_agglomerative_cluster_analysis.py
+++ hmm/hmm_agglomerative_cluster_analysis.py
@@ -6,39 +6,51 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
+import glob
 
-genefile = '1k_genes.p'
+genefile = '3k_genes.p'
 gc, mt, track = load_data()
 genes = load(open(genefile,  'r'))
 data = gc.data.loc[genes, :]
 
-linkage_path = '/'.join(sys.argv[1].split('/') + ['linkage'])
-Z = load(open(linkage_path, 'r'))
+directories = glob.glob('/'.join(sys.argv[1].split('/')))
+print directories
 
-hierarchy.dendrogram(Z)
-dendrogram_file = '/'.join(sys.argv[1].split('/') + ['dendrogram.png'])
-plt.savefig(dendrogram_file)
+for directory in directories:
+    linkage_path = '/'.join(directory.split('/') + ['linkage.p'])
+    cid = directory.split('/')[-1]
+    out_directory = '/'.join(sys.argv[3].split('/') + [cid])
 
-k = int(sys.argv[2])
-fclust = hierarchy.fcluster(Z, k, 'maxclust')
-fclust -= 1
-clustering = {}
-for i in range(0, k):
-    clustering[i] = []
-    for j in np.where(fclust == i)[0]:
-        clustering[i] = clustering[i] + [data.index[j]]
+    Z = load(open(linkage_path, 'r'))
 
-# write cluster assignments
-out_directory = '/'.join(sys.argv[1].split('/'))
-if not os.path.isdir(out_directory):
-    os.makedirs(out_directory)
-filepath = out_directory.split('/') + ['agglomerative_clustering_assignments.txt']
-filepath = '/'.join(filepath)
-with open(filepath, 'w') as f:
-    for cluster, members in clustering.iteritems():
-        f.write(str(cluster))
-        f.write('\n')
-        f.write('\t'.join(members))
-        f.write('\n')
-        f.write('\n')
-        f.write('\n')
+    hierarchy.dendrogram(Z)
+    if not os.path.isdir(out_directory):
+        os.makedirs(out_directory)
+    dendrogram_file = '/'.join(out_directory.split('/') + ['dendrogram.png'])
+    plt.savefig(dendrogram_file)
+
+    k = int(sys.argv[2])
+    fclust = hierarchy.fcluster(Z, k, 'maxclust')
+    fclust -= 1
+    clustering = {}
+    for i in range(0, k):
+        clustering[i] = []
+        for j in np.where(fclust == i)[0]:
+            clustering[i] = clustering[i] + [data.index[j]]
+
+    # write cluster assignments
+    if not os.path.isdir(out_directory):
+        os.makedirs(out_directory)
+    filepath = '/'.join(out_directory.split('/') + ['assignments.p'])
+    dump(clustering, open(filepath, 'w'))
+
+    filepath = out_directory.split('/') + ['assignments.txt']
+    filepath = '/'.join(filepath)
+    with open(filepath, 'w') as f:
+        for cluster, members in clustering.iteritems():
+            f.write(str(cluster))
+            f.write('\n')
+            f.write('\t'.join(members))
+            f.write('\n')
+            f.write('\n')
+            f.write('\n')
